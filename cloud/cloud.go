787eaccdadec7191fed12db6915f94cb9a81718d
Sai Nageswar S <sainageswar@gmail.com>
2025-06-28 05:26:17 +0000

    Feature: Adding ensure bucket to make sure storage exists

--- cloud/cloud.go
+++ cloud/cloud.go
@@ -10,4 +10,5 @@ type Cloud interface {
 	UploadBuffer(ctx context.Context, bucketName, path string, fileData []byte) (string, error)
 	GetPresignedUrl(ctx context.Context, bucketName, path, contentType string, expiry time.Duration) (string, string)
 	DownloadFile(ctx context.Context, bucketName, path string) (string, error)
+	EnsureBucket(ctx context.Context, bucketName string) error
 }
